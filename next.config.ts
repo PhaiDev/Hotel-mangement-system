ec1cf497452e319778836c8e84c9dadac1de4984
PhaiDev <pchaypxm@gmail.com>
2026-04-21 09:30:45 +0700

    Remove allowedDevOrigins from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.163', '172.20.10.3'],
 };
 
 export default nextConfig;
